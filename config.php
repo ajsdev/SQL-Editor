916c9f089db62e984f66add96946e9615b8bc409
Andrew S <andrews54757@gmail.com>
2017-01-26 21:50:06 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,8 @@
+<?php
+$CONFIG["dbHost"] = "127.0.0.1";
+$CONFIG["dbPort"] = "1433";
+$CONFIG["dbUsername"] = "Andrew";
+$CONFIG["dbPassword"] = "Andrewisawesome";
+$CONFIG["dbName"] = "andrews_sql";
 
+?>
